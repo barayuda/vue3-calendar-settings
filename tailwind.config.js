7402fef6a925bddb1b1117687776bbe84f71dace
Bara Yuda <520463+barayuda@users.noreply.github.com>
2024-09-13 16:15:51 +0700

    feat: update tailwind config on vite.config and add flowbite plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+import flowbite from 'flowbite/plugin'
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+    './node_modules/flowbite/**/*.js'
+  ],
   theme: {
     extend: {}
   },
-  plugins: []
+
+  plugins: [flowbite]
 }
